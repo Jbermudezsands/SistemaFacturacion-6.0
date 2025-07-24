9f6fad7f774cbf43257704be506727cbcb605101
Juan Bermudez <jbermudez.sands@gmail.com>
2025-07-24 09:57:57 -0600

    Reparacion en Proceso,....REPORTE EXISTENCIA LOTES

--- Sistema Facturacion/FrmReportes.Designer.vb
+++ Sistema Facturacion/FrmReportes.Designer.vb
@@ -257,7 +257,7 @@ Partial Class FrmReportes
         Me.Button8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button8.Image = CType(resources.GetObject("Button8.Image"), System.Drawing.Image)
         Me.Button8.ImageAlign = System.Drawing.ContentAlignment.TopCenter
-        Me.Button8.Location = New System.Drawing.Point(770, 419)
+        Me.Button8.Location = New System.Drawing.Point(770, 417)
         Me.Button8.Name = "Button8"
         Me.Button8.Size = New System.Drawing.Size(75, 66)
         Me.Button8.TabIndex = 188
